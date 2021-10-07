c8142c55dc4462a847b96c60a0f38045d28b12c0
Esu0 <jshintaro1018@gmail.com>
2021-10-07 22:53:34 +0900

    音楽のパス変更

--- title.cpp
+++ title.cpp
@@ -6,8 +6,8 @@ void titlemain() {
 	AddFontResourceEx("./asset/font/fonts/otf/Mplus1-Regular.otf", FR_PRIVATE, NULL);	//フォントを読み込む
 	int FontHandle = CreateFontToHandle("Mplus1-Regular", 40, 3, DX_FONTTYPE_ANTIALIASING_EDGE_8X8);
 	int PADInput = GetJoypadInputState(DX_INPUT_KEY_PAD1);
-	int bgm = LoadSoundMem("./asset/bgm/maou_minzoku9.mp3");	//魔王魂民族09
-	int effect = LoadSoundMem("./asset/effect/system49.mp3");	//システム音
+	int bgm = LoadSoundMem("./asset/bgm/maou_minzoku9.ogg");	//魔王魂民族09
+	int effect = LoadSoundMem("./asset/effect/system49.ogg");	//システム音
 	DrawStringToHandle(250, 240 - 32, "Zキーで金魚を遊べます\nXキーで射的ができます", GetColor(0,0,0),FontHandle);
 	PlaySoundMem(bgm, DX_PLAYTYPE_LOOP);
 	while (1) {
