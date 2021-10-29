7d1c9cdaf1e7335627b7a3619e53e8cc1b9a98e8
Esu0 <jshintaro1018@gmail.com>
2021-10-29 21:04:33 +0900

    射的と金魚すくいのランキングを表示できるようにした

--- syatekimain.cpp
+++ syatekimain.cpp
@@ -10,16 +10,18 @@ void syatekimain(int font, int bgm, int effect, int calling_check) {
 
 	std::random_device seed;//乱数生成器
 	std::mt19937_64 mt(seed());
-	std::uniform_int_distribution<> dice(1, 1000);
+	std::uniform_int_distribution<> dice;
 
 	int shot = LoadSoundMem("./asset/effect/break.ogg");
 
 	std::vector<int> button_handle{};//ボタン
 	makeImageHandle(button_handle, "./asset/image/start.png", "./asset/image/start.png");
 	std::vector<int> gun_handle{};//aim
-	makeImageHandle(gun_handle, "./asset/image/aim.png");	//TODO:画像を置換する
+	makeImageHandle(gun_handle, "./asset/image/aim.png");
 	std::vector<int> keihin_handle{};//
 	makeImageHandle(keihin_handle, "./asset/image/mato1.png", "./asset/image/mato2.png", "./asset/image/mato3.png", "./asset/image/break.png");
+	std::vector<int> button_back_handle{}; // 戻るボタン
+	makeImageHandle(button_back_handle, "./asset/image/back.png", "./asset/image/back.png"); // 戻るボタン
 
 	unsigned int keihin_num = 10; //景品の数
 	Goldfish keihin(900, 400, true, keihin_handle);
@@ -27,7 +29,11 @@ void syatekimain(int font, int bgm, int effect, int calling_check) {
 	ObjGroup<Goldfish> keihin_group; //景品
 	std::vector<int> keihin_frames(keihin_num); //景品の経過フレームに関する変数
 	keihin_group.addcpy(keihin, keihin_num);
-
+	std::string username;
+	InputText input_username(500, 300, 40);
+	Ranking shateki_ranking("./asset/result/shateki_result.csv");
+	Ranking kingyo_ranking("./asset/result/kingyo_result.csv");
+	std::vector<std::string> tmp;
 
 	//タイトル画面のボタンの配置
 	int px, py;
@@ -35,9 +41,9 @@ void syatekimain(int font, int bgm, int effect, int calling_check) {
 	Button button_start(300, 500, button_handle);
 	Button button_result(550, 500, button_handle);	//設定ボタン
 	Button button_gotokingyo(750, 500, button_handle);	//射的ゲームへ行くボタン
+	Button button_back(520, 400, button_back_handle); // 戻るボタン
 
-
-	KeyInput input(KEY_INPUT_Z);
+	KeyInput input({ KEY_INPUT_Z, KEY_INPUT_RIGHT, KEY_INPUT_LEFT });
 
 	prevtime = GetNowHiPerformanceCount();
 	Timer timer(900);
@@ -50,7 +56,11 @@ void syatekimain(int font, int bgm, int effect, int calling_check) {
 	int count_Font_big = CreateFontToHandle("Mplus1-Regular", 400, 3, DX_FONTTYPE_ANTIALIASING_EDGE_8X8);
 	int count_Font_small = CreateFontToHandle("Mplus1-Regular", 40, 3, DX_FONTTYPE_ANTIALIASING_EDGE_8X8);
 	
-
+	/* 初期化処理 */
+	shateki_ranking.readAll();
+	kingyo_ranking.readAll();
+	input_username.NmlStr = GetColor(255, 255, 255);
+	input_username.NmlCur = GetColor(255, 255, 255);
 	if (calling_check == 0) PlaySoundFile("./asset/bgm/maou_minzoku9.ogg", DX_PLAYTYPE_LOOP); // bgmを読み込む
 
 	/* ゲームループ */
@@ -60,6 +70,7 @@ void syatekimain(int font, int bgm, int effect, int calling_check) {
 
 		GetMousePoint(&px, &py);
 		click_event = GetMouseInputLog2(&button_type, &cx, &cy, &log_type);
+		input();
 
 		if (ProcessMessage() == -1) break;	//エラーが起きたらループをぬける
 
@@ -90,7 +101,8 @@ void syatekimain(int font, int bgm, int effect, int calling_check) {
 					keihin_group.Tag(i) = 0;
 				}
 				timer.reset();
-				windowFlag = 5;	//射的ゲーム待機スタート
+				timer2.reset();
+				windowFlag = 4;	//ユーザー名入力
 			}
 			if (button_result.isReleasedLeft(click_event, button_type, cx, cy, log_type)) {
 				windowFlag = 2;	//結果表示
@@ -103,7 +115,6 @@ void syatekimain(int font, int bgm, int effect, int calling_check) {
 			SetMainWindowText("射的ゲーム(ゲーム中)");	//windowテキスト
 			DrawGraph(0, 0, back_img, TRUE);
 
-			input();
 			gun.next();
 
 			gun.draw();
@@ -120,9 +131,8 @@ void syatekimain(int font, int bgm, int effect, int calling_check) {
 
 			if (input.GetKeyDown(KEY_INPUT_Z)) {
 				/* zキーが押された */
-				SetMainWindowText("射的ゲーム(タイトル)");	//windowテキスト
 				for (int i = 0; i < (int)keihin_num; i++) {
-					if (keihin_group[i].isCought(gun, mt, dice) && keihin_group[i].state < 3) {
+					if (keihin_group[i].isCought(gun) && keihin_group[i].state < 3) {
 						PlaySoundMem(shot, DX_PLAYTYPE_BACK);
 						keihin_group[i].state = 3;
 						score++;
@@ -132,28 +142,92 @@ void syatekimain(int font, int bgm, int effect, int calling_check) {
 
 			//60秒たったら終了
 			if (timer() == 0) {
-				SetMainWindowText("スコア表示");	//windowテキスト
-				if (score > 0) {
-					DrawFormatString(500, 200, GetColor(120, 120, 120), "%d打ち抜けました!", score, font);
-				}
-				else {
-					DrawFormatString(500, 200, GetColor(120, 120, 120), "残念!", font);
-				}
-				if (timer2() == 0) {
-					windowFlag = 0;
-				}
-				timer2.update();
+				windowFlag = 3; // 60秒たったら終了しスコア表示へ
+				shateki_ranking.insert(username, score);
 			}
 			else {
-				DrawFormatStringToHandle(500, 50, GetColor(120, 120, 120), count_Font_small, "残り%d秒", timer() / 60);
+				DrawFormatStringToHandle(500, 50, GetColor(120, 120, 120), count_Font_small, "%d", timer() / 60);
 				DrawFormatStringToHandle(1100, 550, GetColor(120, 120, 120), count_Font_small, "%d", score);
 			}
 			timer.update();
 		}
-		else if (windowFlag == 3) {
-			SetMainWindowText("結果");	//windowテキスト
+		else if (windowFlag == 2)
+		{
+			/* 金魚すくいのランキング */
+			DrawGraph(0, 0, title_img, TRUE);
+			DrawGraph(0, 0, back_black, TRUE);
+			DrawFormatStringToHandle(400, 10, GetColor(255, 255, 0), font, "金魚すくいランキングTOP15");
+			for (unsigned int i = 0; i < 15 && i < kingyo_ranking.column_num(); ++i)
+			{
+				tmp = kingyo_ranking.readAt(i);
+				DrawFormatStringToHandle(200, 40 * (i + 1) + 10, GetColor(255, 255, 255), font, (std::to_string(i + 1) + "位").c_str());
+				DrawFormatStringToHandle(550, 40 * (i + 1) + 10, GetColor(255, 255, 255), font, (tmp[0] + " 様").c_str());
+				DrawFormatStringToHandle(900, 40 * (i + 1) + 10, GetColor(255, 255, 255), font, (tmp[1] + "点").c_str());
+			}
+			if (click_event != -1)
+			{
+				windowFlag = 0;
+			}
+			if (input.GetKeyDown(KEY_INPUT_LEFT))
+			{
+				windowFlag = 6;
+			}
 		}
-
+		else if (windowFlag == 6)
+		{
+			/* 射的ゲームのランキング */
+			DrawGraph(0, 0, title_img, TRUE);
+			DrawGraph(0, 0, back_black, TRUE);
+			DrawFormatStringToHandle(400, 10, GetColor(255, 255, 0), font, "射的ランキングTOP15");
+			for (unsigned int i = 0; i < 15 && i < shateki_ranking.column_num(); ++i)
+			{
+				tmp = shateki_ranking.readAt(i);
+				DrawFormatStringToHandle(200, 40 * (i + 1) + 10, GetColor(255, 255, 255), font, (std::to_string(i + 1) + "位").c_str());
+				DrawFormatStringToHandle(550, 40 * (i + 1) + 10, GetColor(255, 255, 255), font, (tmp[0] + " 様").c_str());
+				DrawFormatStringToHandle(900, 40 * (i + 1) + 10, GetColor(255, 255, 255), font, (tmp[1] + "点").c_str());
+			}
+			if (click_event != -1)
+			{
+				windowFlag = 0;
+			}
+			if (input.GetKeyDown(KEY_INPUT_RIGHT))
+			{
+				windowFlag = 2;
+			}
+		}
+		else if (windowFlag == 3) {
+			DrawGraph(0, 0, back_img, TRUE);
+			DrawGraph(0, 0, back_black, TRUE);
+			SetMainWindowText("結果");	//windowテキスト
+			DrawBoxAA(320, 180, 320 * 3, 180 * 3, GetColor(218, 165, 32), true);
+			button_back.draw();
+			DrawFormatStringToHandle(520, 200, GetColor(120, 120, 120), font, (username + "の結果").c_str());
+			DrawFormatStringToHandle(520, 350, GetColor(120, 120, 120), font, "スコア： %d", score);
+			DrawFormatStringToHandle(590, 425, GetColor(120, 120, 120), font, "戻る");
+			if (score > 0) {
+				DrawFormatStringToHandle(520, 250, GetColor(120, 120, 120), font, "%d個打ち抜けました!", score);
+			}
+			else {
+				DrawFormatStringToHandle(520, 250, GetColor(120, 120, 120), font, "残念!");
+			}
+			if (button_back.isReleasedLeft(click_event, button_type, cx, cy, log_type)) {
+				PlaySoundMem(effect, DX_PLAYTYPE_BACK);
+				windowFlag = 0; // スタート画面へ戻る
+			}
+			timer2.update();
+		}
+		else if (windowFlag == 4)
+		{
+			/* ユーザー名入力 */
+			DrawGraph(0, 0, title_img, TRUE);
+			DrawGraph(0, 0, back_black, TRUE);
+			input_username.draw();
+			if (input_username.entered())
+			{
+				windowFlag = 5;
+				input_username.text(username);
+			}
+		}
 		else if (windowFlag == 5) {
 			DrawGraph(0, 0, back_img, TRUE);
 			DrawGraph(0, 0, back_black, TRUE);
